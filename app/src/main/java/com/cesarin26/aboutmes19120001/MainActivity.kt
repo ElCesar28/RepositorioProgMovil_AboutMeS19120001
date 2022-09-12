1862a89fb6ffb8d75d1a0177f81bc8d8c7b52669
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2022-09-11 23:32:04 -0500

    Initial commit - Aplicación terminada

---
+++ app/src/main/java/com/cesarin26/aboutmes19120001/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cesarin26.aboutmes19120001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
